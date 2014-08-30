347a62e11b1f1b968549c8c974d08cd786d7095f
Julie Torres <reeddetorres@gmail.com>
2014-08-30 18:11:25 -0400

    Add methods and views for creating new todo_items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,4 +2,27 @@ class TodoItemsController < ApplicationController
   def index
     @todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def new
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list.todo_item.new
+  end
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list.todo_item.create(todo_item_params)
+
+    if @todo_item.save
+      flash[:success] = "Added Todo List Item"
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "Todo item could not be added."
+      render action: :new
+    end
+  end
+
+  private
+    def todo_item_params
+      params[:todo_item].permit(:content)
+    end
 end
---
+++ spec/features/todo_items/create_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "Adding ToDo Items" do
+  let!(:todo_list) {TodoList.create(title:       "Groceries"
+                                    description: "Grocery List")}
+
+  def visit_todo_list(list)
+    visit '/todo_lists'
+
+    within 'todo_list_#{list.id}' do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful when adding valid todo items" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "Milk"
+    click_button "Save"
+
+    expect(page).to have_content("Added Todo List Item")
+
+    within("ul.todo_items") do
+      expect(page).to have_content("Milk")
+    end
+  end
+end
