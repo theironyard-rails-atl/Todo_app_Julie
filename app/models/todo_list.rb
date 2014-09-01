a2e608b45fb1e1acddd75f38b95c01d2eacd7376
Julie Torres <reeddetorres@gmail.com>
2014-09-01 17:21:16 -0400

    Update todo list show page

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,13 @@
 class TodoList < ActiveRecord::Base
   has_many :todo_items
   validates :title, presence: true, length: {minimum: 3}
-  validates :description, presence: true, length: {minimum: 5}  
+  validates :description, presence: true, length: {minimum: 5}
+
+  def has_complete_items?
+    todo_items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+    todo_items.incomplete.size > 0
+  end
 end
--- spec/features/todo_lists_spec.rb
+++ spec/features/todo_lists_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe "TodoLists" do
-  describe "GET /todo_lists" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get todo_lists_path
-      response.status.should be(200)
-    end
-  end
+  it {should have_many (:todo_items)}
+
+#TODO: need test for complete items
 end
