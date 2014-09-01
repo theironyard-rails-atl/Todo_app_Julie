404c779ecd1607b947828caffc4e6b4138cec725
Julie Torres <reeddetorres@gmail.com>
2014-09-01 11:00:05 -0400

    Still unable to save new todo items

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-RSpec.describe TodoItem, :type => :model do
-  it {should have_many(:todo_items) }
+describe TodoItem do
+  it {should belong_to(:todo_list)}
+
+  describe "#completed?" do
+    let(:todo_item) {TodoItem.create(content: "Hello")}
+
+    it "is false when completed_at? is blank" do
+      todo_item.completed_at = nil
+      expect(todo_item.completed?).to be_false
+    end
+
+    it "returns true when completed_at? is filled in" do
+      todo_item.completed_at = Time.now
+      expect(todo_item.completed?).to be_true
+    end
+  end
 end
