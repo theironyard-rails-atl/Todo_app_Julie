1aa6c126da85deda98a5ea0cce94fc0084c472d9
Julie Torres <reeddetorres@gmail.com>
2014-09-01 15:19:11 -0400

    Update CSS & view for todo_list & add complete/incomplete scope in todo_item.rb

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,6 +2,10 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   validates :content, presence: true, length: {minimum: 2}
 
+  scope :complete, -> {where("completed_at is not null")}
+  scope :incomplete, -> {where(completed_at: nil)}
+
+
   def completed?
     !completed_at.blank?
   end
